203a06fb187832c4ebadfee2057f79fa3d10287e
susverwimp <sus.verwimp@hotmail.com>
2017-10-02 03:03:24 +0200

    create_board and check for sames horizontal and vertical
    Can create a board so that there are no 3 or more blocks with the same color horizontal or vertical. This is good for an initial board.

--- programming/test3.pl
+++ programming/test3.pl
@@ -1,13 +1,26 @@
 color(red).
 color(green).
-color(blue).
-color(yellow).
+%color(blue).
+%color(yellow).
 
 %%%%%%%%%%%%%%%%%
 % UTILITY PREDICATES
 %%%%%%%%%%%%%%%%%
 append([],L,L). 
 append([H|T],L2,[H|L3])  :-  append(T,L2,L3).
+
+transpose(Ls, Ts) :-
+	lists_transpose(Ls, Ts).
+
+lists_transpose([], []).
+lists_transpose([L|Ls], Ts) :-
+	maplist(same_length(L), Ls),
+	foldl(transpose_, L, Ts, [L|Ls], _).
+
+transpose_(_, Fs, Lists0, Lists) :-
+	maplist(list_first_rest, Lists0, Fs, Lists).
+	
+list_first_rest([L|Ls], L, Ls).
 
 %%%%%%%%%%%%%%%%%
 % GAME PREDICATES
@@ -37,19 +50,43 @@ find_same_horizontal([Row|Tail], Same, Acc) :-
 	find_same_in_list(Row, SameInRow),
 	append(SameInRow, Acc, NewAcc),
 	find_same_horizontal(Tail, Same, NewAcc).
-
+	
+find_same_vertical(Board, Same) :-
+	transpose(Board, ColumnBoard),
+	find_same_horizontal(ColumnBoard, Same, []).
 	
 find_same_in_list(List, Same) :-
-	find_same_in_list(List, Same, [], 0).
-find_same_in_list([], Same, Same, Length).
-find_same_in_list([Block|Tail], Same, Acc, Length) :-
-	
-	find_same_in_list(Tail, Same, Acc).
-	
+	find_same_in_list(List, Same, [], []).
+find_same_in_list([], Same, Acc, TotalAcc) :-
+	length(Acc, Length),
+	Length > 2,
+	append(Acc, TotalAcc, Same).
+find_same_in_list([], Same, Acc, TotalAcc) :-
+	length(Acc, Length),
+	Length =< 2,
+	Same = TotalAcc.
+find_same_in_list([Block|Tail], Same, [], TotalAcc) :-
+	find_same_in_list(Tail, Same, [Block], TotalAcc).
+find_same_in_list([block(Color1, X1, Y1)|Tail], Same, [block(Color2, X2, Y2)|TailAcc], TotalAcc) :-
+	Color1 == Color2,
+	NewAcc = [block(Color1, X1, Y1),block(Color2, X2, Y2)|TailAcc],
+	find_same_in_list(Tail, Same, NewAcc, TotalAcc).
+find_same_in_list([block(Color1, X1, Y1)|Tail], Same, [block(Color2, X2, Y2)|TailAcc], Acc) :-
+	Color1 \= Color2,
+	length([block(Color2, X2, Y2)|TailAcc], Length),
+	Length > 2,
+	append([block(Color2, X2, Y2)|TailAcc], Acc, NewAcc),
+	find_same_in_list(Tail, Same, [block(Color1, X1, Y1)], NewAcc).
+find_same_in_list([block(Color1, X1, Y1)|Tail], Same, [block(Color2, X2, Y2)|TailAcc], Acc) :-
+	Color1 \= Color2,
+	length([block(Color2, X2, Y2)|TailAcc], Length),
+	Length =< 2,
+	find_same_in_list(Tail, Same, [block(Color1, X1, Y1)], Acc).
 
-game(Board, Width, Height) :-
+start(Board, Width, Height) :-
 	create_board(Width, Height, Board),
 	find_same_horizontal(Board, []),
 	find_same_vertical(Board, []).
+	
 	
 query(create_board(2, 2, Board)).
